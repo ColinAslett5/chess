a6b0cc3c95fc48bd00aa1d24fb27a297fa526341
Colin Aslett <280929@bsd48.org>
2017-07-19 10:13:17 -0700

    vector time

--- title.cpp
+++ title.cpp
@@ -1,271 +1,200 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-void board();// the board
-void setup();//setting up the starting positions
-int score();//calculates score, positive score is good for white, negative good for black
-string move();//computer decides what piece to move
-char white[8][8];//char array for the white pieces, will keep the location of all white pieces
-char black[8][8];//char array for the black pieces, will keep the location of all black pieces
-//checking all possible moves for certain piece types
-string Pawn(int a, int b);
-string Rook(int a, int b);
-string Bishop(int a, int b);
-string Knight(int a, int b);
-string Queen(int a, int b);
-string King(int a, int b);
-//turn calculations
-int turn = 0;//0 = white, 1 = black
-bool Kingsafe();//have to make sure the moves dont induce a check
-//global strings for notation purposes
-string letter[] = {"H","G","F","E","D","C","B","A"};//might want to make this global
-string number[] = {"1","2","3","4","5","6","7","8"};
+string board[8][8];
+int turn = 0;//0 is white, 1 is black
+void setup();
+void PossibleMoves();
+struct Node{//a new node will be put in the vector for every move possible
+  int a;//original position a
+  int b;//original position b
+  int newa;//new a position
+  int newb;//new b position
+};
+vector<Node*> list;
+void Pawn(int i,int b);
+void Rook(int i,int b);
+void Knight(int i,int b);
+void Bishop(int i,int b);
+void King(int i,int b);
+void Queen(int i,int b);
+void print();
 int main(){
-  //initially setting all squares to NULL
-  for(int i = 0;i < 8;i++){
-    for(int b = 0;b < 8;b++){
-      white[i][b] = 'x';
-      black[i][b] = 'x';
-    }
-  }
   setup();
-  board();
-  cout << "Possible Moves: " << move() << endl;
-  cout << "Evaluation: " << score();
+  PossibleMoves();
+  print();
   return 0;
 }
-//printing out the board
-void board(){
-  for(int i = 7;i > -1;i--){
-    for(int b = 7;b > -1;b--){
-      if(white[i][b] != 'x'){
-	cout << white[i][b];
-      }
-      else if(black[i][b] != 'x'){
-	cout << black[i][b];
-      }
-      else{
-	cout << black[i][b];
-      }
-    }
-    cout << endl;
+//printing out stuff for debugging purposes
+void print(){
+  int size = list.size();
+  for(int i = 0;i < size;i++){
+    cout << list[i]->a << list[i]->b << list[i]->newa << list[i]->newb << endl;
   }
 }
 //setting up the board
 void setup(){
-  //white
-  for(int i = 0;i < 8;i++){
-    white[1][i] = 'P';
-  }
-  white[2][0] = 'R';
-  white[0][7] = 'R';
-  white[0][1] = 'H';
-  white[0][6] = 'H';
-  white[0][2] = 'B';
-  white[0][5] = 'B';
-  white[0][3] = 'Q';
-  white[0][4] = 'K';
-  //black
-  for(int i = 0;i < 8;i++){
-    black[6][i] = 'p';
-  }
-  black[7][0] = 'r';
-  black[7][7] = 'r';
-  black[7][1] = 'h';
-  black[7][6] = 'h';
-  black[7][2] = 'b';
-  black[7][5] = 'b';
-  black[7][3] = 'q';
-  black[7][4] = 'k';
-}
-//score calculations
-int score(){
-  //looks at pieces first
-  int wsc = 0;//whites score
-  int bsc = 0;//blacks score
-  for(int i = 0;i < 8;i++){
-    for(int b = 0;b < 8;b++){
-      if(white[i][b] == 'P'){
-	wsc++;
-      }
-      else if(white[i][b] == 'H' || white[i][b] == 'B'){
-	wsc += 3;
-      }
-      else if(white[i][b] == 'R'){
-	wsc += 5;
-      }
-      else if(white[i][b] == 'Q'){
-	wsc += 8;
-      }
+  for(int i = 7;i > -1;i--){
+    for(int b = 7;b > -1;b--){
+      board[i][b] = "x";
     }
   }
+  //setting up the board for white
+  for(int i = 0;i < 8;i++){
+    board[1][i] = "P";
+  }
+  board[0][0] = "R";
+  board[0][1] = "K";
+  board[0][2] = "B";
+  board[0][3] = "Q";
+  board[2][4] = "A";
+  board[0][5] = "B";
+  board[0][6] = "K";
+  board[0][7] = "R";
+  //setting up the board for black
+  for(int i = 0;i < 8;i++){
+    board[6][i] = "p";
+  }
+  board[6][0] = "r";
+  board[6][1] = "k";
+  board[6][2] = "b";
+  board[6][3] = "q";
+  board[6][4] = "a";
+  board[6][5] = "b";
+  board[6][6] = "k";
+  board[6][7] = "r";
+}
+void PossibleMoves(){
   for(int i = 0;i < 8;i++){
     for(int b = 0;b < 8;b++){
-      if(black[i][b] == 'p'){
-        bsc++;
+      if(turn == 0){
+	if(board[i][b] == "R"){
+	  //Rook(i,b);
+	}
+	if(board[i][b] == "K"){
+	  //Knight(i,b);
+	}
+	if(board[i][b] == "B"){
+	  //Bishop(i,b);
+	}
+	if(board[i][b] == "Q"){
+	  //Queen(i,b);
+	}
+	if(board[i][b] == "A"){
+	  King(i,b);
+	}
+	if(board[i][b] == "P"){
+	  //Pawn(i,b);
+	}
       }
-      else if(black[i][b] == 'h' || black[i][b] == 'b'){
-        bsc += 3;
-      }
-      else if(black[i][b] == 'r'){
-        bsc += 5;
-      }
-      else if(black[i][b] == 'q'){
-        bsc += 8;
+      else{//blacks turn
+	
       }
     }
   }
-  return wsc-bsc;
 }
-//what move to do?
-string move(){
-  string list = " ";//contains the list of all possible moves
-  //deciding who moves
-  for(int i = 0;i < 8;i++){
-    for(int b = 0;b < 8;b++){
-      if(white[i][b] == 'P'){
-	list = list+Pawn(i,b);
-      }
-      else if(white[i][b] == 'H'){
-	list = list+Knight(i,b);
-      }
-      else if(white[i][b] == 'B'){
-	//list = list+Bishop(i,b);
-      }
-      else if(white[i][b] == 'R'){
-	list = list+Rook(i,b);
-      }
-      else if(white[i][b] == 'Q'){
-	list = list+Queen(i,b);
-      }
-      else if(white[i][b] == 'K'){
-	list = list+King(i,b);
-      }
-    }
+//pawn possible move
+void Pawn(int a, int b){
+  if(board[a+1][b] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a+1;
+    name->newb = b;
+    list.push_back(name);
   }
-  return list;
+  if(board[a+2][b] == "x" && board[a+1][b] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a+2;
+    name->newb = b;
+    list.push_back(name);
+  }
+  //also captures, two diagonals
+  if(board[a+1][b-1] == "p" || board[a+1][b-1] == "r" || board[a+1][b-1] == "b" || board[a+1][b-1] == "k" || board[a+1][b-1] == "q"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a+1;
+    name->newb = b-1;
+    list.push_back(name);
+  }
+  if(board[a+1][b+1] == "p" || board[a+1][b+1] == "r" || board[a+1][b+1] == "b" || board[a+1][b+1] == "k" || board[a+1][b+1] == "q"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a+1;
+    name->newb = b+1;
+    list.push_back(name);
+  }
 }
-//pawn moves
-string Pawn(int a, int b){//strings need spaces when they are initialized
-  string rtstr = " ";//return string
-  if(white[a+1][b] == 'x' && black[a+1][b] == 'x' && Kingsafe()){
-    //rtstr = rtstr + letter[a+1] + to_string(b);
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b] + number[a+1];
+//King moving
+void King(int a,int b){
+  if(board[a+1][b-1] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a+1;
+    name->newb = b-1;
+    list.push_back(name);
   }
-  if(white[a+2][b] == 'x' && black[a+2][b] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b] + number[a+2];
+  if(board[a+1][b] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a+1;
+    name->newb = b;
+    list.push_back(name);
   }
-  return rtstr;
+  if(board[a+1][b+1] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a+1;
+    name->newb = b+1;
+    list.push_back(name);
+  }
+  if(board[a][b-1] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a;
+    name->newb = b-1;
+    list.push_back(name);
+  }
+  if(board[a][b+1] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a;
+    name->newb = b+1;
+    list.push_back(name);
+  }
+  if(board[a-1][b-1] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a-1;
+    name->newb = b-1;
+    list.push_back(name);
+  }
+  if(board[a-1][b] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a-1;
+    name->newb = b;
+    list.push_back(name);
+  }
+  if(board[a-1][b+1] == "x"){
+    Node* name = new Node();
+    name->a = a;
+    name->b = b;
+    name->newa = a-1;
+    name->newb = b+1;
+    list.push_back(name);
+  }
+  //castling and captures and kingcheck
 }
-string Rook(int a, int b){
-  string rtstr = " ";
-  //right to left
-  for(int i = b+1;i < 8;i++){
-    if(white[a][i] == 'x' && black[a][i] == 'x' && Kingsafe()){
-      rtstr = rtstr + letter[b] + number[a] + "->" + letter[i] + number[a];
-    }
-    else{
-      break;
-    }
-  }
-  return rtstr;
-}
-string Bishop(int a, int b){
-  string rtstr = " ";
-  return rtstr;
-}
-string Knight(int a, int b){
-  string rtstr = " ";
-  if(white[a+2][b+1] == 'x' && black[a+2][b+1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b+1] + number[a+2];
-  }
-  if(white[a+2][b+1] == 'x' && black[a+2][b-1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b-1] + number[a+2];
-  }
-  if(white[a+1][b+2] == 'x' && black[a+1][b+2] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b+2] + number[a+1];
-  }
-  if(white[a+1][b-2] == 'x' && black[a+1][b-2] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b-2] + number[a+1];
-  }
-  if(white[a-2][b+1] == 'x' && black[a-2][b+1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b+1] + number[a-2];
-  }
-  if(white[a-2][b-1] == 'x' && black[a-2][b-1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b-1] + number[a-2];
-  }
-  return rtstr;
-}
-string Queen(int a, int b){
-  string rtstr = " ";
-  //right movement
-  for(int i = b+1;i < 8;i++){
-    if(white[a][i] == 'x' && black[a][i] == 'x' && Kingsafe()){
-      rtstr = rtstr + letter[b] + number[a] + "->" + letter[i] + number[a];
-    }
-    else{//there is a piece in the way
-      break;
-    }
-  }
-  //left movement
-  for(int i = b-1;i > -1;i--){
-    if(white[a][i] == 'x' && black[a][i] == 'x' && Kingsafe()){
-      rtstr = rtstr + letter[b] + number[a] + "->" + letter[i] + number[a];
-    }
-    else{
-      break;
-    }
-  }
-  //Up movement
-  for(int i = a+1;i < 8;i++){
-    if(white[i][b] == 'x' && black[i][b] == 'x' && Kingsafe()){
-      rtstr = rtstr + letter[b] + number[a] + "->" + letter[b] + number[i];
-    }
-    else{
-      break;
-    }
-  }
-  //down movement
-  for(int i = a-1;i > -1;i--){
-    if(white[i][b] == 'x' && black[i][b] == 'x' && Kingsafe()){
-      rtstr = rtstr + letter[b] + number[a] + "->" + letter[b] + number[i];
-    }
-    else{
-      break;
-    }
-  }
-  return rtstr;
-}
-string King(int a, int b){
-  string rtstr = " ";
-  //eight possible moves
-  if(white[a+1][b-1] == 'x' && black[a+1][b-1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b-1] + number[a+1];
-  }
-  if(white[a+1][b] == 'x' && black[a+1][b] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b] + number[a+1];
-  }
-  if(white[a+1][b+1] == 'x' && black[a+1][b+1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b+1] + number[a+1];
-  }
-  if(white[a][b-1] == 'x' && black[a][b-1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b-1] + number[a];
-  }
-  if(white[a][b+1] == 'x' && black[a+1][b+1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b+1] + number[a];
-  }
-  if(white[a-1][b-1] == 'x' && black[a-1][b-1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b-1] + number[a-1];
-  }
-  if(white[a-1][b] == 'x' && black[a-1][b] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b] + number[a-1];
-  }
-  if(white[a-1][b+1] == 'x' && black[a-1][b+1] == 'x' && Kingsafe()){
-    rtstr = rtstr + letter[b] + number[a] + "->" + letter[b+1] + number[a-1];
-  }
-  //castling also
-  return rtstr;  
-}
-//have to make sure the king is not in check when a move is made
-bool Kingsafe(){
-  return true;
-}
+
